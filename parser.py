5357496e3aafd487ac2b0f655d69311aadb1a2d5
jarusified <jarus3001@gmail.com>
2017-04-27 13:41:21 -0700

    generate blocks into data file

--- parser.py
+++ parser.py
@@ -19,6 +19,7 @@ for i in range(1,len(f)):
     else:
         g.append(split_f)
 
+print len(f)
 # Creating the blocks
 blocks = []
 block = []
@@ -43,12 +44,17 @@ def shrink(str):
             tmp.append(i)
     return tmp
 
+h = file("parsed_data.csv", "w")
+for i in range(0, len(blocks)):
+    print str(blocks[i]) +"\n" 
+    h.write(str(blocks[i]) + "\n")
+   
 #parsing the blocks
 for i in range(0, len(blocks)):
     t_call = []
     for j in range(0, len(blocks[i])):
        if blocks[i][j][1] == '"MPI_Irecv"' or  blocks[i][j][1] == '"MPI_Isend"' :
            t_call.append(blocks[i][j][11])
-       if blocks[i][j][0] == "MPI_Waitall":
-           print shrink(blocks[i][j][1])
-    print map(int, t_call)
+#       if blocks[i][j][0] == "MPI_Waitall":
+           #print shrink(blocks[i][j][1])
+    #print map(int, t_call)
